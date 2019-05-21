e8d2160e2029328891634ce374d360cd1cbf8e38
anameilani <meilani.ana@gmail.com>
2019-05-21 23:56:53 +0700

    sudah bisa register-login

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express= require('express')
 const app= express()
 const port= 3000
+const customerRoute = require('./router/customerRoute')
+
 
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', (req, res)=>{
+    res.render('index.ejs')
+})
+
+app.use('/customer', customerRoute)
 
 app.listen(port, () => console.log(`Server running on port port: ${port}`))
--- models/customer.js
+++ models/customer.js
@@ -1,14 +1,48 @@
 'use strict';
+const bcrypt= require('bcrypt')
+const salt= bcrypt.genSaltSync(10)
+
 module.exports = (sequelize, DataTypes) => {
   const Customer = sequelize.define('Customer', {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     birthday: DataTypes.DATE,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate:{
+        isEmail:{
+          args: true,
+          msg: 'Please input email in correct format'
+        },
+        isUniq(value){
+          return Customer.findOne({where: { email: value }})
+          .then(email=>{
+            if(email) throw new Error ('Email has been used')
+          })
+          .catch(err=>{
+            throw new Error (err.message)
+          })
+        }
+      }
+    },
+    password: {
+      type:DataTypes.STRING,
+      validate:{
+        len:{
+          args: [8,12],
+          msg: 'Less password length. Password length 8-12 character'
+        }
+      }
+    },
     point: DataTypes.INTEGER,
     balance: DataTypes.INTEGER
-  }, {});
+  }, {
+    hooks:{
+      beforeSave: (customer, options) => {
+        customer.password = bcrypt.hashSync(customer.password, salt)
+      }
+    }
+  });
   Customer.associate = function(models) {
     // associations can be defined here
   };
